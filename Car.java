73790083617e788d82937826ba8fd3eab79862d5
fmendo23 <fmendozer@yahoo.com>
2026-04-23 03:41:03 +0000

    Add Car program

--- Car.java
+++ Car.java
@@ -30,8 +30,8 @@ public class Car {
 
     public void printInfo() {
         System.out.println("Car's information:");
-        System.out.println(" Model year: " + modelYear);
-        System.out.println(" Purchase price: $" + purchasePrice);
-        System.out.println(" Current value: $" + currentValue);
+        System.out.println("  Model year: " + modelYear);
+        System.out.println("  Purchase price: $" + purchasePrice);
+        System.out.println("  Current value: $" + currentValue);
     }
 }
