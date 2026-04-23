31ffecaa365e084154441d4e90cd44be7f9ba6f9
fmendo23 <fmendozer@yahoo.com>
2026-04-23 03:29:58 +0000

    Add Car program

---
+++ Car.java
@@ -0,0 +1,37 @@
+import java.lang.Math;
+
+public class Car {
+    private int modelYear;
+    private int purchasePrice;
+    private int currentValue;
+
+    public void setModelYear(int userYear){
+        modelYear = userYear;
+    }
+
+    public int getModelYear() {
+        return modelYear;
+    }
+
+    public void calcCurrentValue(int currentYear) {
+        double depreciationRate = 0.15;
+        int carAge = currentYear - modelYear;
+
+        currentValue = (int) Math.round(purchasePrice * Math.pow((1 - depreciationRate), carAge));
+    }
+
+    public void setPurchasePrice(int price) {
+        purchasePrice = price;
+    }
+
+    public int getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void printInfo() {
+        System.out.println("Car's information:");
+        System.out.println(" Model year: " + modelYear);
+        System.out.println(" Purchase price: $" + purchasePrice);
+        System.out.println(" Current value: $" + currentValue);
+    }
+}
